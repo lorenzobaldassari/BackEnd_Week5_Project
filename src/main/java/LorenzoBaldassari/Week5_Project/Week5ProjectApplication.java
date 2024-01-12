574fa4cd35553a51c9aaacf87786b1230d163320
Lorenzo Baldassari <lorebalda93@gmail.com>
2024-01-12 09:16:00 +0100

    Initial commit

---
+++ src/main/java/LorenzoBaldassari/Week5_Project/Week5ProjectApplication.java
@@ -0,0 +1,13 @@
+package LorenzoBaldassari.Week5_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week5ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LorenzoBaldassari/Week5_Project/Week5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package LorenzoBaldassari.Week5_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
